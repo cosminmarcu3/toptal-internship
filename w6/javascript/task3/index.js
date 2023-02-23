ddb38d6eddb36fe629048616d7b491e72cc54d75
Cosmin Marcu <cosmin.marcu@toptal.com>
2023-02-23 16:35:45 +0200

    Fixed comments

--- w6/javascript/task3/index.js
+++ w6/javascript/task3/index.js
@@ -5,11 +5,25 @@ const movieList = [];
 
 const movieFactory = ({ title, year, genres, posterUrl }) => {
   const currentMovie = movieTemplate.content.cloneNode(true);
+  const poster = currentMovie.querySelector(".movie_poster");
 
-  currentMovie.querySelector(".movie_title").innerText = title;
-  currentMovie.querySelector(".movie_year").innerText = `(${year})`;
-  currentMovie.querySelector(".movie_genres").innerText = genres.join(", ");
-  currentMovie.querySelector(".movie_poster").src = posterUrl;
+  currentMovie.querySelector(".movie_title").textContent = title;
+
+  currentMovie.querySelector(".movie_year").textContent = `(${year})`;
+
+  currentMovie.querySelector(".movie_genres").append(
+    ...genres.map((genre) => {
+      const genreItem = document.createElement("li");
+      genreItem.textContent = genre;
+      return genreItem;
+    })
+  );
+
+  poster.src = posterUrl;
+  poster.addEventListener(
+    "error",
+    ({ currentTarget }) => (currentTarget.src = "./defaultposter.jpg")
+  );
 
   return currentMovie.querySelector(".movie_item");
 };
